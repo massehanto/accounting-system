9c036be7ec76e1cf678e25efdc77f97984c88cd3
massehanto <massehanto@gmail.com>
2025-09-17 16:29:20 +0700

    Cleanup commit

--- frontend/src/pages/mod.rs
+++ frontend/src/pages/mod.rs
@@ -6,7 +6,7 @@ pub mod journal_entries;
 pub mod company_management;
 pub mod accounts_payable;
 pub mod accounts_receivable;
-pub mod inventory;
+pub mod inventory_management;
 pub mod tax_management;
 pub mod reports;
 
@@ -17,6 +17,6 @@ pub use journal_entries::*;
 pub use company_management::*;
 pub use accounts_payable::*;
 pub use accounts_receivable::*;
-pub use inventory::*;
+pub use inventory_management::*;
 pub use tax_management::*;
 pub use reports::*;
